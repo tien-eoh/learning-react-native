d394de4ef39ed715c64d870a11b3d49b2bdaabbe
tientran <tien.tbtt@eoh.io>
2021-09-27 21:33:19 +0700

    screen 1-2

---
+++ foodSource/screens/onboarding/SplashScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Image, StyleSheet, View} from 'react-native';
+
+export const SplashSCreen = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.background}
+        source={require('../../images/Pattern.png')}
+      />
+      <View style={[styles.background, styles.view]}>
+        <Image style={styles.logo} source={require('../../images/Logo.png')} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  view: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 188,
+    height: 203,
+  },
+});
